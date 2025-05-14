b4f11b562a69d4381fb523ee2bdf3e9a647fb22a
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-05-14 01:41:56 +0000

    new

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const text = "Welcome to Convert My Time!";
       document.getElementById("typed-text").textContent += text.charAt(i);
       i++;
       setTimeout(typeWriter, 200);
-    }
+    } else
+    document.getElementById("cursor").style.display = "none";
   }
 
   window.onload = typeWriter;
