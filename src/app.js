a9abb50e83125827111f58cebbd56d24e46da288
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-05-14 00:59:51 +0000

    new

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const text = "Welcome to Convert My Time!"; // this is the text it will type
+const text = "Welcome to Convert My Time!"; 
   let i = 0;
 
   function typeWriter() {
     if (i < text.length) {
       document.getElementById("welcome").innerHTML += text.charAt(i);
       i++;
-      setTimeout(typeWriter, 100); // how fast it types (100ms per letter)
+      setTimeout(typeWriter, 100); 
     }
   }
 
-  window.onload = typeWriter; // start typing when the page loads
+  window.onload = typeWriter; 
