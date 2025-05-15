59a25c4b28cce294552ec65a54b55b23a4a52b42
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-05-15 22:34:32 +0000

    new

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const text = "Welcome to Convert My Time!";
-let images =  ["/Images/glenn-carstens-peters-ZWD3Dx6aUJg-unsplash.jpg", "/Images/tird.jpg", "/Images/people.jpg", "/Images/andres.jpg", "/Images/carlos.jpg", "/Images/airport-2373727_1280.jpg", "/Images/pret.jpg", "/Images/building.jpg"];
+let images =  ["/Images/glenn-carstens-peters-ZWD3Dx6aUJg-unsplash.jpg", "/Images/tird.jpg", "/Images/people.jpg", "/Images/andres.jpg", "/Images/carlos.jpg", "/Images/airport-2373727_1280.jpg", "/Images/pret.jpg", "/Images/building.jpg", "/Images/map.jpg"];
 let heroImage = document.getElementById("hero-image");
 const timeElement = document.getElementById("time");
 const now = new Date();
