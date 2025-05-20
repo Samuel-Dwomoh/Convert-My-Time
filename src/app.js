4d6f823b7d49953f8304fbe62d1048f8c6203b11
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-05-20 12:31:15 +0000

    new

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const text = "Welcome to Convert My Time!";
 const time = document.getElementById("time");
+let timeToRealTime = [];
+timeToRealTime.push(time);
 let images =  ["/Images/glenn-carstens-peters-ZWD3Dx6aUJg-unsplash.jpg", "/Images/tird.jpg", "/Images/people.jpg", "/Images/andres.jpg", "/Images/carlos.jpg", "/Images/airport-2373727_1280.jpg", "/Images/pret.jpg", "/Images/building.jpg", "/Images/map.jpg"];
 let heroImage = document.getElementById("hero-image");
 const timeElement = document.getElementById("time");
@@ -42,10 +44,21 @@ let currentIndex = 0;
       heroImage.classList.remove("opacity-0");
     }, 0);
   }, 9000); 
-
-
-function calculate(){
   
+  
+  if(time.length < 4 || time.length > 4 || time[2] !=":"){
+   alert("Please enter valid time with format HH:MM");
+  } else {
+    return 1;
+  }
+
+function convertTImeToDate(){
+  /** timeToRealTime = time.number();
+  timeToRealTime.split("");
+  timeToRealTIme[1].textContent(":");
+  timeToRealTime.join();
+  console.log(timeToRealTime);
+  **/
 }
 
   window.onload = typeWriter;
